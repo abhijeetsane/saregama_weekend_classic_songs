b08fc242b371047f09143ec446996f9deab73b62
Abhijeet Anant Sane <abhijeet.sane@gmail.com>
2024-01-14 20:44:51 +0530

    Update download.sh
    In the past couple of years the RSS had changed and no longer my script worked , Found the new URL and changed the grep a little.
    
    
    best luck.

--- download.sh
+++ download.sh
@@ -2,12 +2,14 @@ CURL="/usr/bin/curl"
 XML_FILE_NAME="weekend_radio.xml"
 DOWNLOAD_LIST="files_to_download.lst"
 AXEL="/usr/bin/axel"
-URL="http://r.saregama.com/resources/podcast/weekend_radio.xml"
+#URL="http://r.saregama.com/resources/podcast/weekend_radio.xml"
+URL="https://anchor.fm/s/151cdc88/podcast/rss"
 echo "Starting download of Saragama Weekend classic songs"
 $CURL $URL > $XML_FILE_NAME
-cat  $XML_FILE_NAME | grep guid  | sed -e 's/.*<guid>\(.*\)<\/guid>.*/\1/'  > $DOWNLOAD_LIST
+#cat  $XML_FILE_NAME | grep guid  | sed -e 's/.*<guid>\(.*\)<\/guid>.*/\1/'  > $DOWNLOAD_LIST
+cat $XML_FILE_NAME | grep guid | sed -e 's/.*<guid isPermaLink="false">\(.*\)<\/guid>.*/\1/' > $DOWNLOAD_LIST
 for file in `cat $DOWNLOAD_LIST`; do
     echo "Downloading : $file"
-    $AXEL -qa $file 
+    $AXEL -qa "$file"
 done
 echo "Download of current songs listed in Saregama Classic completed"
